38ae9b443abd189f9aa82e64e95a83eb9106e623
AnoyingShulkerMC <44982417+AnoyingShulkerMC@users.noreply.github.com>
2022-10-02 11:18:38 -0700

    even more changes

--- applicationCommands/debug.js
+++ applicationCommands/debug.js
@@ -2,6 +2,6 @@
 import utils from "node:util"
 export default async function (interaction, { options }, context) {
   interaction.respond(4, {
-    content: eval(options.statement).toString()
+    content: "" + eval(options.statement)
   })
 }
