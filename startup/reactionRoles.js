d7b93357bbce9c85bf97ba01da5eb087ca1ccef4
AnoyingShulkerMC <44982417+AnoyingShulkerMC@users.noreply.github.com>
2022-10-02 21:15:02 -0700

    Cache guild members.

--- startup/reactionRoles.js
+++ startup/reactionRoles.js
@@ -1,2 +1,25 @@
-export default async function ({api, con, guilds}) {
+import Database from "@replit/database"
+import APIManager from "../lib/APIManager"
+import GuildManager from "../lib/cache/GuildManager"
+import GatewayConnection from "../lib/GatewayConnection"
+/**
+ * 
+ * @param {Object} context
+ * @param {GuildManager} context.guilds
+ * @param {APIManager} context.api
+ * @param {GatewayConnection} context.con
+ */
+export default async function ({ api, con, guilds }) {
+	var db = new Database()
+	for (var [item, guild] of guilds.items) {
+		var reactRoles = JSON.stringify(db.get(item)).reactRoles
+		for (i of reactRoles) {
+			con.on("MESSAGE_REACTION_ADD", (react) => {
+				if (react.message_id !== i.msg_id) return
+				if (react.emoji.name !== i.name || react.emoji.id !== i.id) return
+				var role = await guild.roles.get(i.role)
+
+			})
+		}
+	}
 }
