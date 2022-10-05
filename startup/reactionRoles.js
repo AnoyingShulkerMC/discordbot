ec076af961c91a45590894ebff39f838951126c6
AnoyingShulkerMC <44982417+AnoyingShulkerMC@users.noreply.github.com>
2022-10-04 19:20:44 -0700

    modifyReaction Roles

--- startup/reactionRoles.js
+++ startup/reactionRoles.js
@@ -13,46 +13,43 @@ import GatewayConnection from "../lib/GatewayConnection.js"
 export default async function ({ api, con, guilds }) {
 	await sleep(1000) // Wait for guilds to resolve
 	var db = new Database()
-	db.set("713917232580919376", JSON.stringify({ reactRoles: [{ role: "730168714313596929", msg_id: "1023638692268613652", guild_id: "713917232580919376", name: "true", id: "866138928959717377"  }] })).then(() => { });
-	for (var [item, guild] of guilds.items) {
-		try {
-			var reactRoles = JSON.parse(await db.get(item)).reactRoles || []
-			for (var i of reactRoles) {
-				con.on("MESSAGE_REACTION_ADD", async (react) => {
-					if (react.message_id !== i.msg_id) return
-					if (react.guild_id !== guild.id) return
-					if (react.emoji.name !== i.name || react.emoji.id !== i.id) return
-					var role = await guild.roles.get(i.role)
-					var member = (await guild.members.get(con.applicationID))
-					var highestRole = member.roles.map(a => guild.roles.items.get(a)).sort((a, b) => b.position - a.position)[0]
-					if (!(member.permissions & (1 << 28)) || role.position >= highestRole.position) return
-					api.sendRequest({
-						endpoint: `/guilds/${guild.id}/members/${react.user_id}/roles/${i.role}`,
-						method: "PUT",
-						additionalHeaders: {
-							"x-audit-log-reason": `Reaction Role for MessageID ${react.message_id}`
-						}
-					})
-				})
-				con.on("MESSAGE_REACTION_REMOVE", async (react) => { 
-					if (react.message_id !== i.msg_id) return
-					if (react.guild_id !== guild.id) return
-					if (react.emoji.name !== i.name || react.emoji.id !== i.id) return
-					var role = await guild.roles.get(i.role)
-					var member = (await guild.members.get(con.applicationID))
-					var highestRole = member.roles.map(a => guild.roles.items.get(a)).sort((a, b) => b.position - a.position)[0]
-					if (!(member.permissions & (1 << 28)) || role.position >= highestRole.position) return
-					api.sendRequest({
-						endpoint: `/guilds/${guild.id}/members/${react.user_id}/roles/${i.role}`,
-						method: "DELETE",
-						additionalHeaders: {
-							"x-audit-log-reason": `Reaction Role for MessageID ${react.message_id}`
-						}
-					})
-				})
-			}
-		} catch (e) { console.log(e) }
-	}
+	db.set("713917232580919376", JSON.stringify({ reactRoles: [ { msgID: "1023638692268613652", role: "730168714313596929", name: "true", id: "866138928959717377" }] })).then(() => { });
+	con.on("MESSAGE_REACTION_ADD", async (react) => {
+		var dbEntry = db.get(react.guild_id).reactRoles
+		for (var i of Object.entries(dbEntry)) {
+			if (react.message_id !== msgID) continue
+			if (react.emoji.name !== i.name || react.emoji.id !== i.id) return
+			var role = await guild.roles.get(i.role)
+			var member = (await guild.members.get(con.applicationID))
+			var highestRole = member.roles.map(a => guild.roles.items.get(a)).sort((a, b) => b.position - a.position)[0]
+			if (!(member.permissions & (1 << 28)) || role.position >= highestRole.position) return
+			api.sendRequest({
+				endpoint: `/guilds/${guild.id}/members/${react.user_id}/roles/${i.role}`,
+				method: "PUT",
+				additionalHeaders: {
+					"x-audit-log-reason": `Reaction Role for MessageID ${react.message_id}`
+				}
+			})
+		}
+	})
+	con.on("MESSAGE_REACTION_REMOVE", async (react) => { 
+		var dbEntry = db.get(react.guild_id).reactRoles
+		for (var i of Object.entries(dbEntry)) {
+			if (react.message_id !== msgID) continue
+			if (react.emoji.name !== i.name || react.emoji.id !== i.id) return
+			var role = await guild.roles.get(i.role)
+			var member = (await guild.members.get(con.applicationID))
+			var highestRole = member.roles.map(a => guild.roles.items.get(a)).sort((a, b) => b.position - a.position)[0]
+			if (!(member.permissions & (1 << 28)) || role.position >= highestRole.position) return
+			api.sendRequest({
+				endpoint: `/guilds/${guild.id}/members/${react.user_id}/roles/${i.role}`,
+				method: "DELETE",
+				additionalHeaders: {
+					"x-audit-log-reason": `Reaction Role for MessageID ${react.message_id}`
+				}
+			})
+		}
+	})
 }
 function sleep(mils) {
 	return new Promise(resolve => setTimeout(resolve, mils))
